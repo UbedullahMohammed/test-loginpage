e2afae6a0fa3d737ee5c12678e6388df16a0e862
Mohammed Ubedullah <mohammed_ub@photonint.com>
2022-11-22 16:02:54 +0530

    ready to deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Login from "./components/Login";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test the App Component", () => {
+  test("header renders in the document", () => {
+    const component = render(<App />);
+    const linkElement = component.getByText(/React Bootstrap Login Form/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test("render login component in doucment", () => {
+    const { getByLabelText } = render(<Login />);
+    const childElement = getByLabelText("Email Address");
+    expect(childElement).toBeTruthy();
+  });
 });
